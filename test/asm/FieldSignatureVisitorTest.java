2ac752efc41e6424443f3794f69cccedb21818df
Philip Ross <rosspa@rose-hulman.edu>
2016-01-12 20:54:28 -0500

    wrote some tests for the field signature visitor

---
+++ test/asm/FieldSignatureVisitorTest.java
@@ -0,0 +1,36 @@
+package asm;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+import org.junit.Test;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.signature.SignatureReader;
+
+public class FieldSignatureVisitorTest {
+	@Test
+	public void testSimpleFieldSignatureVisitor() throws IOException {
+		assertFieldSignature("Ljava/util/ArrayList<Ljava/lang/Integer;>;",
+				new HashSet<String>(Arrays.asList("java.lang.Integer")));
+	}
+
+	@Test
+	public void testComplicatedFieldSignatureVisitor() throws IOException {
+		assertFieldSignature("Ljava/util/HashMap<Ljava/util/ArrayList<Ljava/lang/String;>;Ljava/lang/Integer;>;",
+				new HashSet<String>(Arrays.asList("java.lang.String", "java.lang.Integer")));
+	}
+
+	public void assertFieldSignature(String signature, HashSet<String> expectedResult) throws IOException {
+		SignatureReader reader = new SignatureReader(signature);
+		FieldSignatureVisitor visitor = new FieldSignatureVisitor(Opcodes.ASM5);
+
+		assertTrue(visitor.getFields().isEmpty());
+		reader.accept(visitor);
+		assertEquals(expectedResult, visitor.getFields());
+	}
+}
