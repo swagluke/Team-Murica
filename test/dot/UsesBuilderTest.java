e7a28ad5ff43f72194e79f8dd4713eb264cb4bd2
Philip Ross <rosspa@rose-hulman.edu>
2016-01-14 05:05:44 -0500

    added non functioning uses test

---
+++ test/dot/UsesBuilderTest.java
@@ -0,0 +1,22 @@
+package dot;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Test;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.Type;
+
+import dot.records.MethodRecord;
+
+public class UsesBuilderTest {
+	@Test
+	public void test() {
+		// TODO
+		fail();
+	}
+}
