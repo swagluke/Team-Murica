8084c8ef0fdbab41633473f7e2560519d9816eaf
Luke Zhang <zhangl@rose-hulman.edu>
2016-02-05 12:31:05 -0500

    Updated DecoratorTest

--- test/dot/DecoratorTest.java
+++ test/dot/DecoratorTest.java
@@ -1,16 +1,19 @@
 package dot;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 
+import gui.UmlWrapper;
 import org.junit.Test;
 
 import records.ClassRecord;
+import records.IClassRecord;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
 
 /**
  * Created by lukezhang on 1/29/16.
@@ -19,37 +22,74 @@ public class DecoratorTest
 {
     @Test
     public void testNotDecorator() throws IOException, ClassNotFoundException {
-        String className="";
-        String expectedUml = "";
-        /*String className = "dot.AssociationBuilder";
-        String expectedUml = "AssociationBuilder [label = \"{AssociationBuilder|+visitor : asm.ClassFieldSignatureVisitor\\l\n"
-                + "|+ applyPatternrecords.ClassRecord  : void\\l\n"
-                + "+ getVisitor : org.objectweb.asm.ClassVisitor\\l\n" + "}\"]";*/
-        assertDecorator(className, false, expectedUml);
+        assertDecorator(new String[] { "java.util.Enumeration", "java.util.Iterator" },
+                new String[0], new String[0], new String[0], "");
     }
 
     @Test
     public void testIsDecorator() throws IOException, ClassNotFoundException {
-        String className="";
-        String expectedUml = "";
-        /*String className = "singleton.Singleton";
-        String expectedUml = "Singleton [color = \"blue1\" label = \"{Singleton\\n\\<\\<Singleton\\>\\>|+instance : singleton.Singleton\\l\n"
-                + "|+ getInstance : singleton.Singleton\\l\n" + "}\"]Singleton -> Singleton\n";
-                */
-        assertDecorator(className, true, expectedUml);
+        String[] classnames = new String[] { "problem.client.IteratorToEnumerationAdapter", "java.util.Iterator",
+                "java.util.Enumeration" };
+        String expectedUml = "digraph G {fontname = \"Comic Sans MS\"  fontsize = 8  node [ fontname = \"Comic Sans MS\" fontsize = 8 shape = \"record\"] edge [ fontname = \"Comic Sans MS\" fontsize = 8 ]\n"
+                + "IteratorToEnumerationAdapter [color = \"red\" label = \"{IteratorToEnumerationAdapter\\n\\<\\<Adapter\\>\\>|+itr : java.util.Iterator\\l\n"
+                + "|+ nextElement : java.lang.Object\\l\n" + "+ hasMoreElements : boolean\\l\n" + "}\"]\n"
+                + "IteratorToEnumerationAdapter -> Iterator[label=\"<<adapts>>\"]edge [ arrowhead = \"empty\" style = \"dotted\"]\n"
+                + "IteratorToEnumerationAdapter -> Enumeration\n"
+                + "Iterator [color = \"red\" label = \"{Iterator\\n\\<\\<Adaptee\\>\\>||+ forEachRemainingjava.util.function.Consumer  : void\\l\n"
+                + "+ hasNext : boolean\\l\n" + "+ next : java.lang.Object\\l\n" + "+ remove : void\\l\n"
+                + "}\"]"
+                + "Enumeration [color = \"red\" label = \"{Enumeration\\n\\<\\<Adapter Target\\>\\>||+ hasMoreElements : boolean\\l\n"
+                + "+ nextElement : java.lang.Object\\l\n" + "}\"]"
+                + "}";
+        assertDecorator(classnames, new String[] { "problem.client.IteratorToEnumerationAdapter" },
+                new String[] { "java.util.Enumeration" }, new String[] { "java.util.Iterator" }, expectedUml);
     }
 
-    private void assertDecorator(String className, boolean expectedIsDecorator, String expectedUml)
-    {
-    	fail();
-        DecoratorBuilder builder = new DecoratorBuilder(new UmlBuilder(className,new HashSet<String>(Arrays.asList(className))));
-        ClassRecord record = (ClassRecord) builder.build();
-        assertTrue(expectedIsDecorator == record.getBaseRecord().getPatternNames().contains("Decorator"));
-        if (expectedIsDecorator) {
-            assertEquals(expectedUml, record.getClassUml());
-        } else {
-            assertEquals(new UmlBuilder(className, new HashSet<String>(Arrays.asList(className))).build().getClassUml(),
-                    record.getClassUml());
+    public void assertDecorator(String[] classNames, String[] adapterClasses, String[] targetClasses,
+                              String[] adapteeClasses, String expectedUml) {
+        try {
+            UmlWrapper umlWrapper = new UmlWrapper(classNames);
+            umlWrapper.addBuilderClass(ExtensionBuilder.class);
+            umlWrapper.addBuilderClass(ImplementsBuilder.class);
+            umlWrapper.addBuilderClass(AdapterBuilder.class);
+            String actualUml = umlWrapper.build();
+            // System.out.println(actualUml);
+            HashMap<String, IClassRecord> recordMap = umlWrapper.getRecords();
+
+            for (String className : adapterClasses) {
+                ClassRecord record = umlWrapper.getRecords().get(className).getBaseRecord();
+                assertTrue(record.getPatternNames().contains("Decorator"));
+                assertFalse(record.getPatternNames().contains("Decorator Target"));
+                assertFalse(record.getPatternNames().contains("Decoratee"));
+            }
+
+            for (String className : targetClasses) {
+                ClassRecord record = umlWrapper.getRecords().get(className).getBaseRecord();
+                assertFalse(record.getPatternNames().contains("Decorator"));
+                assertTrue(record.getPatternNames().contains("Decorator Target"));
+                assertFalse(record.getPatternNames().contains("Decoratee"));
+            }
+
+            for (String className : adapteeClasses) {
+                ClassRecord record = umlWrapper.getRecords().get(className).getBaseRecord();
+                assertFalse(record.getPatternNames().contains("Decorator"));
+                assertFalse(record.getPatternNames().contains("Decorator Target"));
+                assertTrue(record.getPatternNames().contains("Decoratee"));
+            }
+
+            System.out.println(actualUml);
+            if (expectedUml.equals("")) {
+                UmlWrapper baseUmlWrapper = new UmlWrapper(classNames);
+                umlWrapper.addBuilderClass(ExtensionBuilder.class);
+                umlWrapper.addBuilderClass(ImplementsBuilder.class);
+                assertEquals(baseUmlWrapper.build(), actualUml);
+            } else {
+                assertEquals(expectedUml, actualUml);
+            }
+        } catch (NoSuchMethodException | SecurityException | InstantiationException | IllegalAccessException
+                | IllegalArgumentException | InvocationTargetException e) {
+            e.printStackTrace();
+            fail();
         }
     }
 }
