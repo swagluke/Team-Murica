5101f73715fe64f1ed79f43e34c6f9fdec2a9387
Jernik <Luke.Miller.DRSS@gmail.com>
2016-02-11 22:57:42 -0500

    added composite detection

--- src/dot/DecoratorBuilder.java
+++ src/dot/DecoratorBuilder.java
@@ -18,15 +18,12 @@ public class DecoratorBuilder extends APatternBuilder {
 
 	@Override
 	public boolean isPattern(IClassRecord record, HashMap<String, IClassRecord> recordMap) {
-//		System.out.println("Checking pattern in decorator");
 		try {
 			boolean hasConstructorAndField = false;
 			boolean extendsDecorator = false;
 			Class<?> thisClass = Class
 					.forName(Type.getObjectType(record.getBaseRecord().getClassName()).getClassName());
 
-			// System.out.println("in is pattern");
-			// System.out.println(record.getBaseRecord().getClassName());
 			HashSet<String> possibles = new HashSet<String>();
 			if (record.canConvertRecord(ExtendedClassRecord.class)) {
 				ExtendedClassRecord extendedClassRecord = (ExtendedClassRecord) record
@@ -39,26 +36,19 @@ public class DecoratorBuilder extends APatternBuilder {
 				possibles.addAll(implementsClassRecord.getImplementsList());
 			}
 			possibles.removeIf(p -> !record.getClassList().contains(p.replace("/", ".")));
-			// System.out.println("possibles: " + "" + possibles);
 
 			for (String possible : possibles) {
-				Class<?> implemntingClass = Class.forName(Type.getObjectType(possible).getClassName());
+				Class<?> implmentingClass = Class.forName(Type.getObjectType(possible).getClassName());
 
 				for (MethodRecord methodRecord : record.getBaseRecord().getMethodsList()) {
 					if (methodRecord.getName().equals("<init>")) {
-						// System.out.println(methodRecord.getName() + ": " +
-						// methodRecord.getStypes());
 						for (String arg : methodRecord.getStypes()) {
 							try {
 								if (Class.forName(arg).isAssignableFrom(thisClass)) {
-									// System.out.println(arg + " is assignable from
-									// " + record.getBaseRecord().getClassName());
 									for (InstanceVarRecord field : record.getBaseRecord().getFieldsList()) {
 										Class<?> fieldClass = Class.forName(field.getType());
 										if (field.getType().equals(arg)) {
 											hasConstructorAndField = true;
-											// return true
-											// System.out.println(true);
 										}
 									}
 								}
@@ -69,55 +59,33 @@ public class DecoratorBuilder extends APatternBuilder {
 					}
 				}
 			}
-			// for (InstanceVarRecord field :
-			// record.getBaseRecord().getFieldsList()) {
-			// String fieldName = field.getType();
-			// System.out.println(fieldName);
-			// }
 			if (!hasConstructorAndField) {
 				ExtendedClassRecord extendedClassRecord = (ExtendedClassRecord) record
 						.tryConvertRecord(ExtendedClassRecord.class);
 				String extendsName = extendedClassRecord.getExtendsName();
-				// System.out.println(recordMap.get(extendsName.replace("/",
-				// ".")));
 				if (recordMap.get(extendsName.replace("/", ".")) != null) {
-					// System.out.println("extends something");
 					if (this.isPattern(recordMap.get(extendsName.replace("/", ".")), recordMap)) {
 						extendsDecorator = true;
-						// System.out.println(record.getBaseRecord().getClassName()
-						// + " extends a decorator");
 					}
 				}
 			}
-			// has constructorfield or extends a decorator
-			// System.out.println("name: " +
-			// record.getBaseRecord().getClassName() + " hasConstructorAndField:
-			// "
-			// + hasConstructorAndField + " extendsDecorator: " +
-			// extendsDecorator);
 			return hasConstructorAndField || extendsDecorator;
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
-//			System.out.println("couldn't find a class");
 			return false;
 		}
 	}
 
 	@Override
 	public void applyPattern(IClassRecord record, HashMap<String, IClassRecord> recordHashMap) {
-//		System.out.println("Apply decorators stuff");
-//		System.out.println(record.getBaseRecord().getClassName() + " is a decorator");
 		record.getBaseRecord().setBoxColor("green");
 		record.getBaseRecord().addPattern("Decorator");
 
 		String extendsName = ((ExtendedClassRecord) record.tryConvertRecord(ExtendedClassRecord.class)).getExtendsName()
 				.replace("/", ".");
-//		System.out.println(extendsName);
 		if (recordHashMap.get(extendsName) != null) {
-			// System.out.println("hello");
 			IClassRecord extendedRecord = recordHashMap.get(extendsName);
 			if (!this.isPattern(extendedRecord, recordHashMap)) {
-//				System.out.println(extendsName + " is the component");
 			}
 		}
 
@@ -135,7 +103,6 @@ public class DecoratorBuilder extends APatternBuilder {
 			                        + implementsName.substring(implementsName.lastIndexOf('.') + 1)
 			                        + "[label=\"<<decorates>>\"]"
 			        );
-//					System.out.println(implementsName + " is the component");
 
 					implementsRecord.getBaseRecord().addPattern("Component");
 				}
