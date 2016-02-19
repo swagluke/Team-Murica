4709f9b57f5e32d5cef289ebe6e2362e065d95f1
Philip Ross <rosspa@rose-hulman.edu>
2016-02-19 09:43:19 -0500

    root recursive checking

--- src/gui/DirectoryPane.java
+++ src/gui/DirectoryPane.java
@@ -1,18 +1,15 @@
 package gui;
 
 import java.awt.BorderLayout;
-import java.awt.Button;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.util.Enumeration;
 
 import javax.swing.Box;
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JTree;
 import javax.swing.event.TreeExpansionEvent;
@@ -32,6 +29,7 @@ public class DirectoryPane extends APanel {
 	private static final long serialVersionUID = -1869406409371334949L;
 	private JTree tree;
 	private Component scrollPane;
+	private DefaultTreeModel treeModel;
 
 	public DirectoryPane(Gui gui) {
 		super(gui);
@@ -39,39 +37,28 @@ public class DirectoryPane extends APanel {
 
 	@Override
 	protected void setUp() {
-//		this.setBorder(BorderFactory.createLineBorder(Color.red, 5));
-//		this.setLayout(new FlowLayout(FlowLayout.LEFT));
+		// this.setBorder(BorderFactory.createLineBorder(Color.red, 5));
+		// this.setLayout(new FlowLayout(FlowLayout.LEFT));
 		this.setLayout(new BorderLayout());
-		this.setUpTree();
-//		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-		this.setUpButton();
+		this.add(this.generateTree());
+		// this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+		this.add(this.generateButton(), BorderLayout.PAGE_END);
 		this.setBackground(Color.white);
 		this.setMinimumSize(new Dimension(200, 500));
 	}
 
-	private void setUpTree() {
+	private JTree generateTree() {
 		String path = "src";
 		File dir = new File("").getAbsoluteFile();
 		File next = new File(dir, path);
 		System.out.println(dir);
 		System.out.println(next.listFiles());
-		final DefaultMutableTreeNode root = new DefaultMutableTreeNode(path);
+		final CheckBoxNodeData data = new CheckBoxNodeData(path, true);
+		final DefaultMutableTreeNode root = new DefaultMutableTreeNode(data);
 		this.walk(root, next);
 
-//		final DefaultMutableTreeNode accessibility = add(root, "Accessibility", true);
-//		add(accessibility, "Move system caret with focus/selection changes", false);
-//		add(accessibility, "Always expand alt text for images", true);
-//		root.add(accessibility);
-//
-//		final DefaultMutableTreeNode browsing = new DefaultMutableTreeNode("Browsing");
-//		add(browsing, "Notify when downloads complete", true);
-//		add(browsing, "Disable script debugging", true);
-//		add(browsing, "Use AutoComplete", true);
-//		add(browsing, "Browse in a new process", false);
-//		root.add(browsing);
-
-		final DefaultTreeModel treeModel = new DefaultTreeModel(root);
-		this.tree = new JTree(treeModel);
+		this.treeModel = new DefaultTreeModel(root);
+		tree = new JTree(treeModel);
 
 		final CheckBoxNodeRenderer renderer = new CheckBoxNodeRenderer();
 		tree.setCellRenderer(renderer);
@@ -103,21 +90,22 @@ public class DirectoryPane extends APanel {
 
 		// listen for changes in the model (including check box toggles)
 		treeModel.addTreeModelListener(new TreeModelListener() {
-
 			@Override
 			public void treeNodesChanged(final TreeModelEvent e) {
 				System.out.println(System.currentTimeMillis() + ": nodes changed");
 				Object[] children = e.getChildren();
-				for (Object obj : children) {
-					DefaultMutableTreeNode c = (DefaultMutableTreeNode) obj;
-					CheckBoxNodeData directoryData = (CheckBoxNodeData) c.getUserObject();
-					boolean isChecked = directoryData.isChecked();
-					
-					Enumeration<DefaultMutableTreeNode> en = c.breadthFirstEnumeration();
-					while (en.hasMoreElements()) {
-						DefaultMutableTreeNode node = en.nextElement();
-						CheckBoxNodeData data = (CheckBoxNodeData) node.getUserObject();
-						data.setChecked(isChecked);
+				if (children == null) {
+					// root
+					DefaultMutableTreeNode blah = (DefaultMutableTreeNode) e.getPath()[0];
+					CheckBoxNodeData data = (CheckBoxNodeData) blah.getUserObject();
+					setChecked(blah, data.isChecked());
+					blah.breadthFirstEnumeration();
+				} else {
+					// non root
+					for (Object obj : children) {
+						DefaultMutableTreeNode node = (DefaultMutableTreeNode) obj;
+						CheckBoxNodeData directoryData = (CheckBoxNodeData) node.getUserObject();
+						setChecked(node, directoryData.isChecked());
 					}
 				}
 				repaint();
@@ -138,19 +126,21 @@ public class DirectoryPane extends APanel {
 				System.out.println(System.currentTimeMillis() + ": structure changed");
 			}
 		});
-		 this.add(tree);
-	}
-	
-	private void setUpButton() {
-		Box box = Box.createHorizontalBox();
-		JButton button = this.generateButton();
-		button.setMaximumSize(new Dimension(197, 25));
-//		button.setPreferredSize(button.getMaximumSize());
-		box.add(button);
-		this.add(box, BorderLayout.PAGE_END);
+		return tree;
 	}
 
-	private JButton generateButton() {
+	private void setChecked(DefaultMutableTreeNode parent, boolean checked) {
+		Enumeration<DefaultMutableTreeNode> en = parent.breadthFirstEnumeration();
+		while (en.hasMoreElements()) {
+			DefaultMutableTreeNode node = en.nextElement();
+			CheckBoxNodeData data = (CheckBoxNodeData) node.getUserObject();
+			data.setChecked(checked);
+		}
+		
+	}
+
+	private Box generateButton() {
+		Box box = Box.createHorizontalBox();
 		JButton button = new JButton("Reanalyze");
 		button.addActionListener(new ActionListener() {
 			@Override
@@ -167,12 +157,13 @@ public class DirectoryPane extends APanel {
 				}).start();
 			}
 		});
-		return button;
+		button.setMaximumSize(new Dimension(197, 25));
+		box.add(button);
+		return box;
 	}
-	
+
 	public void reanalyze(JButton button) {
-		System.out.println("reanalying");
-		button.setText("I'm analyzing");
+		button.setText("TODO I'm analyzing");
 		try {
 			Thread.sleep(1000);
 		} catch (InterruptedException e) {
@@ -181,21 +172,20 @@ public class DirectoryPane extends APanel {
 		}
 	}
 
-
 	private void walk(DefaultMutableTreeNode parent, File file) {
 		File[] files = file.listFiles();
 		if (files == null) {
 			return;
 		}
-		for (File f: files) {
+		for (File f : files) {
 			DefaultMutableTreeNode child = add(parent, f.getName(), true);
 			this.walk(child, new File(file, f.getName()));
 		}
-//		final DefaultMutableTreeNode accessibility = add(root, "Accessibility", true);
-//		add(accessibility, "Move system caret with focus/selection changes", false);
-//		add(accessibility, "Always expand alt text for images", true);
-//		root.add(accessibility);
-		
+		// final DefaultMutableTreeNode accessibility = add(root, "Accessibility", true);
+		// add(accessibility, "Move system caret with focus/selection changes", false);
+		// add(accessibility, "Always expand alt text for images", true);
+		// root.add(accessibility);
+
 	}
 
 	private static DefaultMutableTreeNode add(final DefaultMutableTreeNode parent, final String text,
